289f68e0d1a80401f5f2591843c3f309024f97fd
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-12-02 16:48:11 -0400

    Rewrite Slingitem

--- main_file/lua/FinalDemo/Slingitem.lua
+++ main_file/lua/FinalDemo/Slingitem.lua
@@ -1,62 +1,40 @@
 --Don't use rs weapons, we already have a propper one
 addHook("MobjThinker", function(mo)
-		if G_RingSlingerGametype() then
-			if (mo and mo.valid and mo.target and finaldemo_character[mo.target.skin].slingitem) then
-				P_RemoveMobj(mo)
-				S_StopSound(mo)
-			end
-		end
+	if mo.target
+	and (finaldemo_character[mo.target.skin] and finaldemo_character[mo.target.skin].slingitem) then
+		S_StopSound(mo)
+		P_RemoveMobj(mo)
+	end
 end, MT_REDRING)
 
-addHook("PlayerThink", function(p)
+local function Shoot(p, slingitem)
 	local pmo = p.mo
-	if pmo and pmo.valid and pmo.health and finaldemo_character[pmo.skin].slingitem then
-		p.slingitem = finaldemo_character[p.mo.skin].slingitem --define the missile mobj here
-		if not p.fireholding then -- init fire holding checks to avoid obj spawn spam
-			p.fireholding = false
-		end
-		
-		--Cooldown for the sling item as intended
-		if not p.slingcooldown then
-			p.slingcooldown = 0
-		end
-		if p.slingcooldown > 0 then
-			p.slingcooldown = $-1
-		end
-		
-		if p.rings != 0 then
-			p.slashtarget = P_LookForEnemies(p, false, true)
-			if (p.slashtarget 
-			and p.slashtarget.valid 
-			and p.slashtarget.health) then
-				p.slashangle = R_PointToAngle2(pmo.x, pmo.y, p.slashtarget.x, p.slashtarget.y)
-			end
-			
-			if p.cmd.buttons & BT_ATTACK then
-				if not p.fireholding then
-					if p.slingcooldown == 0 then
-						if p.slashtarget and p.slashtarget.valid then
-							P_SpawnMissile(pmo, p.slashtarget, p.slingitem)
-							if not G_RingSlingerGametype() then
-								p.slingcooldown = TICRATE/4
-							end
-						else
-							P_SpawnPlayerMissile(pmo, p.slingitem)
-							if not G_RingSlingerGametype() then
-								p.slingcooldown = TICRATE/4
-							end
-						end
-						if not G_RingSlingerGametype() then
-							p.rings = $-1
-						end
-					end
-					p.fireholding = true
-				else
-					return
-				end
-			else
-				p.fireholding = false
-			end
+	if not (pmo and pmo.valid) then return end
+
+	p.lookfortargets = P_LookForEnemies(p, false, true)
+	
+	if p.lookfortargets then
+		P_SpawnMissile(pmo, p.lookfortargets, slingitem)
+	else
+		P_SpawnPlayerMissile(pmo, slingitem)
+	end
+end
+
+addHook("PreThinkFrame", function()
+for p in players.iterate() do
+	local pmo = p.mo
+	local cmd = p.cmd
+	if not (pmo and pmo.valid and p.playerstate == PST_LIVE and (finaldemo_character[pmo.skin] and finaldemo_character[pmo.skin].slingitem)) then continue end
+	local slingitem = finaldemo_character[pmo.skin].slingitem
+
+	if p.rings != 0
+	and not p.weapondelay
+	and (cmd.buttons & BT_ATTACK) and not (p.lastbuttons & BT_ATTACK) then
+		Shoot(p, slingitem)
+		if not G_RingSlingerGametype() then
+			p.rings = $-1
+			p.weapondelay = TICRATE/4
 		end
 	end
+end
 end)
