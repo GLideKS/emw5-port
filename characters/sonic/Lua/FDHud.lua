ef2fab664440e657c4ebe0443885269f01d56b26
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-12-03 00:05:32 -0400

    Define EMW Sonic for final demo HUD

---
+++ characters/sonic/Lua/FDHud.lua
@@ -0,0 +1,5 @@
+--add sonic to the final demo hud list from emw5
+if not finaldemo_character then rawset(_G, "finaldemo_character", {}) end
+finaldemo_character["emwsonic"] = {
+	name_graphic = "STSONIC"
+}
