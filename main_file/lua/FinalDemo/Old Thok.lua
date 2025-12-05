552d57cca4be420a4547189db2533ac12076779d
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-12-05 01:25:38 -0400

    Add disp offset to old thok due to sprite clipping

--- main_file/lua/FinalDemo/Old Thok.lua
+++ main_file/lua/FinalDemo/Old Thok.lua
@@ -10,6 +10,7 @@ mobjinfo[MT_OLDTHOK] = {
         spawnhealth = 1,
         radius = 32*FU,
         height = 64*FU,
-        flags = MF_NOBLOCKMAP|MF_NOCLIP|MF_NOCLIPHEIGHT|MF_NOGRAVITY|MF_SCENERY
+        flags = MF_NOBLOCKMAP|MF_NOCLIP|MF_NOCLIPHEIGHT|MF_NOGRAVITY|MF_SCENERY,
+		dispoffset = 40
 }
 states[S_OLDTHOK] = {SPR_THOK, TR_TRANS60|B, 8, nil, nil, nil, nil}
