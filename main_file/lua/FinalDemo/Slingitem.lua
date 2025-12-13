ec98c9f95ae0555133e3da2b4e8da268630d3112
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-12-13 01:10:56 -0400

    Recode slingitem functionality

--- main_file/lua/FinalDemo/Slingitem.lua
+++ main_file/lua/FinalDemo/Slingitem.lua
@@ -3,56 +3,44 @@ Replica of Final Demo's S_SKIN slingitem functionality for EMW5.
 Coded by: GLide KS
 */
 
---we gotta set this up manually in the PreThinkFrame where the inputs goes
-mobjinfo[MT_REDRING].seesound = sfx_none 
+--we gotta play this sound manually if we want to use custom slingitem's seesound
+mobjinfo[MT_REDRING].seesound = sfx_none
 
---Don't use red ring weapon for slingitem characters, already has it's own
+--Shoot the character's slingitem if defined instead
 addHook("PostThinkFrame", function()
 if gamestate != GS_LEVEL then return end
 for mo in mobjs.iterate() do
-	if mo.valid and mo.type == MT_REDRING and mo.target
-	and (finaldemo_character[mo.target.skin] and finaldemo_character[mo.target.skin].slingitem) then
-		P_RemoveMobj(mo)
+	if mo.valid and mo.type == MT_REDRING and mo.target then
+		local has_slingitem = (finaldemo_character[mo.target.skin] and finaldemo_character[mo.target.skin].slingitem)
+		if has_slingitem then
+			P_SpawnPlayerMissile(mo.target, finaldemo_character[mo.target.skin].slingitem)
+			P_RemoveMobj(mo)
+		end
 	end
 end
 end)
 
---Shooting function for slingitems
-local function Shoot(p, slingitem)
-	local pmo = p.mo
-	if not (pmo and pmo.valid) then return end
+--Play the usual ring shoot sound for non-slingitem characters
+addHook("MobjSpawn", function(mo)
+	if (mo.target and (finaldemo_character[mo.target.skin] and finaldemo_character[mo.target.skin].slingitem)) then return end
+	S_StartSound(mo, sfx_thok)
+end, MT_REDRING)
 
-	P_SpawnPlayerMissile(pmo, slingitem)
-end
-
---Main hook to handle slingitem shooting
-addHook("PreThinkFrame", function()
-if gamestate != GS_LEVEL then return end
-for p in players.iterate() do
-	if p.exiting then continue end
+--Do ringslinger in non-ringslinger gamemodes for slingitem characters
+addHook("PlayerThink", function(p)
+	if not (p and p.mo and p.mo.valid and p.playerstate == PST_LIVE and p.rings) then return end
+	if G_RingSlingerGametype() then return end
 	local pmo = p.mo
 	local cmd = p.cmd
-	if not (pmo and pmo.valid and p.playerstate == PST_LIVE) then continue end
 	local has_slingitem = finaldemo_character[pmo.skin] and finaldemo_character[pmo.skin].slingitem
+	if not has_slingitem then return end
 	
-	if p.rings != 0
-	and not p.weapondelay then
-		if (cmd.buttons & BT_ATTACK) and not (p.lastbuttons & BT_ATTACK) then
-			if has_slingitem then
-				local slingitem = finaldemo_character[pmo.skin].slingitem
-				Shoot(p, slingitem)
-				if not G_RingSlingerGametype() then
-					p.rings = $-1
-					p.weapondelay = TICRATE/4
-				end
-			elseif G_RingSlingerGametype() then
-				S_StartSound(pmo, sfx_thok) -- play the shooting sound as usual
-			end
-		elseif (cmd.buttons & BT_FIRENORMAL) and not (p.lastbuttons & BT_FIRENORMAL)
-		and G_RingSlingerGametype()
-		and not has_slingitem then
-			S_StartSound(pmo, sfx_thok) -- play the shooting sound as usual
-		end
+	if not p.weapondelay
+	and ((cmd.buttons & BT_ATTACK) and not (p.lastbuttons & BT_ATTACK))
+	and has_slingitem then
+		local slingitem = finaldemo_character[pmo.skin].slingitem
+		P_SpawnPlayerMissile(pmo, slingitem)
+		p.rings = $-1
+		p.weapondelay = TICRATE/4
 	end
-end
 end)
