30e5a4ab2db12f6b67b1ee0ddc1f43e85f8f1d59
GLide KS <84994993+GLideKS@users.noreply.github.com>
2025-12-21 16:34:26 -0400

    Better code legacy replacements

--- main_file/lua/FinalDemo/Legacy Replacements.lua
+++ main_file/lua/FinalDemo/Legacy Replacements.lua
@@ -1,55 +1,28 @@
---Monitors
+local Object_OldVariant = {
+	--Monitors
+	MT_1UP_BOX = MT_FDBOX_1UP,
+	MT_RING_BOX = MT_FDBOX_RING,
+	MT_ATTRACT_BOX = MT_FDBOX_YELLOW,
+	MT_FORCE_BOX = MT_FDBOX_BLUE,
+	MT_ARMAGEDDON_BOX = MT_FDBOX_BLACK,
+	MT_WHIRLWIND_BOX = MT_FDBOX_WHITE,
+	MT_SNEAKERS_BOX = MT_FDBOX_SHOES,
+	MT_INVULN_BOX = MT_FDBOX_INVIN,
+	MT_EGGMAN_BOX = MT_FDBOX_EGGMAN,
+	MT_MIXUP_BOX = MT_FDBOX_MIXUP,
+	MT_MYSTERY_BOX = MT_FDBOX_QUESTION,
+	MT_ELEMENTAL_BOX = MT_FDBOX_BLUE,
+	--Enemies
+	MT_CRAWLACOMMANDER = MT_OLDCRAWLACOMANDER
+}
 
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_1UP end
-end,MT_1UP_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_RING end
-end,MT_RING_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_YELLOW end
-end,MT_ATTRACT_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_BLUE end
-end,MT_FORCE_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_BLACK end
-end,MT_ARMAGEDDON_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_WHITE end
-end,MT_WHIRLWIND_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_SHOES end
-end,MT_SNEAKERS_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_INVIN end
-end,MT_INVULN_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_EGGMAN end
-end,MT_EGGMAN_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_MIXUP end
-end,MT_MIXUP_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_QUESTION end
-end,MT_MYSTERY_BOX)
-
-addHook("MobjSpawn", function(mo)
-	if mapheaderinfo[gamemap].emw5 then mo.type = MT_FDBOX_BLUE end
-end,MT_ELEMENTAL_BOX)
-
---Enemies
-
-addHook("MobjSpawn", function(mo)
-	if mo.type == MT_CRAWLACOMMANDER and mapheaderinfo[gamemap].emw5 then mo.type = MT_OLDCRAWLACOMANDER end
-end,MT_CRAWLACOMMANDER)
+for i, v in pairs(Object_OldVariant) do
+	print(i)
+	addHook("MobjSpawn", function(mo)
+		if mapheaderinfo[gamemap].emw5 then 
+			mo.type = v 
+			P_SetMobjStateNF(mo, mobjinfo[v].spawnstate)
+			return true
+		end
+	end, _G[i])
+end
