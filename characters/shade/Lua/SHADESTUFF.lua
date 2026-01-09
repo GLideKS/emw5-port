b6178c3531099f0dec231e36c94312ef4fa28a60
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-01-09 13:21:58 -0400

    Prevent shade from climbing

--- characters/shade/Lua/SHADESTUFF.lua
+++ characters/shade/Lua/SHADESTUFF.lua
@@ -63,8 +63,6 @@ states[S_SHADESLINGDEATH2] = {SPR_NULL, A, 30, nil, 0, 0, S_NULL}
 addHook("PlayerThink", function(p)
 	local pmo = p.mo
 	if pmo and pmo.valid and pmo.health and pmo.skin == "emwshade" then
-		--Shade doesn't climb in the original version. Still, I don't know how to avoid the cling
-		p.climbing = 0
 		
 		--In the original version of this character, spin trails dissapears when on super
 		if p.powers[pw_super] then
@@ -87,3 +85,25 @@ FDChar["emwshade"] = {
 	oldglide = true,
 	oldglide_rolloff = true
 }
+
+--Wall checks from Nick WolfFang, prevents Shade to climb
+--Can't find a way to slide on walls still
+local function WallBumping(mo,mobj,line)
+	if mo and mo.valid
+	and mo.skin == "emwshade"
+	and (mo.player.pflags & PF_GLIDING)
+	and not (mobj and mobj.valid and mobj.flags & (MF_MONITOR|MF_ENEMY|MF_BOSS))
+	and line and line.valid then
+
+		if NK_GlideClimbTraverse(mo,line) 
+		and not (line and line.backsector
+		and line.backsector.ceilingpic == "F_SKY1"
+		and line.frontsector
+		and line.frontsector.ceilingpic == "F_SKY1"
+		and (mo.z >= line.frontsector.ceilingheight
+		or mo.z >= line.backsector.ceilingheight)) then
+			return true
+		end
+	end
+end
+addHook("MobjMoveBlocked", WallBumping, MT_PLAYER)
