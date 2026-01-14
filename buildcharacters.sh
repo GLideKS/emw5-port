9576950563a677691e1354c91e22e086e07f2c63
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-01-14 00:52:42 -0400

    Fix filename conventions again

--- buildcharacters.sh
+++ buildcharacters.sh
@@ -1,13 +1,13 @@
-rm out/CL_Amyemw_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_bradEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_EspioEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_KnuxEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_metalemw_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_ShadeEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_shadowEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_SonicEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_TailsEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
-rm out/CL_vectorEMW_GLideKSPort_Srb2-2.2_V0.98.pk3
+rm out/CL_Amyemw_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_bradEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_EspioEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_KnuxEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_metalemw_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_ShadeEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_shadowEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_SonicEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_TailsEMW_GLideKSPort_Srb2-2.2_V1.pk3
+rm out/CL_vectorEMW_GLideKSPort_Srb2-2.2_V1.pk3
 python3 pk3make.py build/vector.txt
 python3 pk3make.py build/tails.txt
 python3 pk3make.py build/sonic.txt
--- buildmain.sh
+++ buildmain.sh
@@ -1,2 +1,2 @@
-rm out/SML_emw5base_GLideKSPort_Srb2-2.2_V0.98.pk3
+rm out/SML_emw5base_GLideKSPort_Srb2-2.2_V1.pk3
 python3 pk3make.py build/main.txt -j 3
