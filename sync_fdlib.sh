21bad2f92c9096a11bbf7907a64ad0407f05fca8
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-04-02 16:01:24 -0400

    Add FD Character Library as a Git Submodule

--- sync_fdlib.sh
+++ sync_fdlib.sh
@@ -1,19 +1,23 @@
 # script to sync a single FD Character Library core files to all EMW5 characters
 #!/bin/bash
 
-FDLIBROOT='Libraries/FDCharLib'
+FDLIB_DIRECTORY='FD-CharacterLib/src' # FD Character Lib's directory in the repository
+FDLIBROOT='Libraries/FDCharLib' # FDLib in each character (Lua folder)
+
 # names MUST match the folder name in characters folder
 CHARACTERS=("amy" "sonic" "vector" "tails" "shadow" "shade" "metal" "knuckles" "espio" "brad")
 
 for CHAR in "${CHARACTERS[@]}"; do
     echo "Synching FD Character Library for : $CHAR..."
+    
+    CHAR_LUADIR="characters/$CHAR/Lua"
 
     mkdir -p "characters/$CHAR/Graphics"
-    mkdir -p "characters/$CHAR/Lua/$FDLIBROOT"
-    cp -r "libraries/FDCharLib/lua/"* "characters/$CHAR/Lua/$FDLIBROOT/"
-    cp -r "libraries/FDCharLib/Graphics" "characters/$CHAR"
-    cp -r "libraries/FDCharLib/init.lua" "characters/$CHAR/Lua/$FDLIBROOT/exec.lua"
-    sed -i "s|FDCharLib_root = \"\"|FDCharLib_root = \"$FDLIBROOT/\"|g" "characters/$CHAR/Lua/$FDLIBROOT/exec.lua"
+    mkdir -p "$CHAR_LUADIR/$FDLIBROOT"
+    cp -r "$FDLIB_DIRECTORY/lua/"* "$CHAR_LUADIR/$FDLIBROOT/"
+    cp -r "$FDLIB_DIRECTORY/Graphics" "characters/$CHAR"
+    cp -r "$FDLIB_DIRECTORY/init.lua" "$CHAR_LUADIR/$FDLIBROOT/exec.lua"
+    sed -i "s|FDCharLib_root = \"\"|FDCharLib_root = \"$FDLIBROOT/\"|g" "$CHAR_LUADIR/$FDLIBROOT/exec.lua"
 done
 
 echo "Completed!"
