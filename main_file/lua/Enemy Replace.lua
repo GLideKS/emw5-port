abbdd82f293ca64d24fa1306ba3725dd3903c9c3
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-28 12:32:25 -0400

    Finish EMW5 Flamethrower as a separated missile

--- main_file/lua/Enemy Replace.lua
+++ main_file/lua/Enemy Replace.lua
@@ -18,11 +18,12 @@ for i, v in pairs(Object_OldVariant) do
 	end, _G[i])
 end
 
---Ammo replacing
-addHook("MapLoad", function()
-    if mapheaderinfo[gamemap].emw5 then
-        states[S_TURRETFIRE].var1 = MT_EMW5_FLAMETHROWER --Turret's laser to flame thrower
-    else
-        states[S_TURRETFIRE].var1 = 0
-    end
-end)
+function A_TurretFire(actor, var1, var2)
+	if actor.type == MT_TURRET --Only replace it's ammo with flame thrower in EMW5 maps.
+	and mapheaderinfo[gamemap].emw5 then
+		super(actor, MT_EMW5_FLAMETHROWER, nil)
+		return
+	end
+
+	super(actor, var1, var2)
+end
